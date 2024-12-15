ba4cce13ecf4ddebdabd3a5cb1a272cd24caad02
Alina <alishaliss09@gmail.com>
2024-12-16 02:41:14 +0300

    верстка карточки продукта

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,13 +1,20 @@
 
+import { Galery } from "src/components/galery/galery"
 import { Footer } from "../components/footer/footer"
 import { Headers } from "../components/header/header"
+import { Info } from "src/components/info/info"
 
 export const Product = () => {
     return (
         <>
             <div className="page">
                 <Headers />
-                <div>PRODUCT PAGE</div>
+                <div className="container">
+                    <div className="products">
+                        <Galery />
+                        <Info />
+                    </div>
+                </div>
                 <Footer />
             </div>
         </>
