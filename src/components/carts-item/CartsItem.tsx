79917b8859a2204e94b144ad32a78ff5c32e45c6
Alina <alishaliss09@gmail.com>
2025-01-09 15:02:32 +0300

    изменила Carts

---
+++ src/components/carts-item/CartsItem.tsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom'
+import styles from './CartsItem.module.scss'
+import boots_white from "@icons/boots_white.png";
+import basket from "@icons/basket.svg";
+import { CountBtn } from '@ui-kit/count-btn';
+import { RedButton } from '@ui-kit/red-button';
+import { useToggleState } from "@hooks/useToggleState";
+
+export const CartsItem = () => {
+    const {
+        state: isDelete,
+        setTrue: handleDelete,
+        setFalse: handleAddToCart,
+    } = useToggleState();
+    return (
+        <div className={styles.cart__form_item}>
+            <Link to="/product/1" className={styles.cart__form_left}>
+                <img
+                    src={boots_white}
+                    alt="boots"
+                    className={styles.cart__form_img}
+                />
+                <div className={styles.cart__form_about}>
+                    <div className={styles.cart__form_about_title}>
+                        Essence Mascara Lash Princess
+                    </div>
+                    <div className={styles.cart__form_about_price}>$110</div>
+                </div>
+            </Link>
+            {!isDelete ? (
+                <>
+                    <CountBtn onResetToCart={handleDelete} />
+                    <button
+                        className={styles.cart__form_delete}
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </button>
+                </>
+            ) : (
+                <button onClick={handleAddToCart}>
+                    <RedButton imageSrc={basket} padding="16px" />
+                </button>
+            )}
+        </div>
+    )
+}
--- src/components/carts/Carts.tsx
+++ src/components/carts/Carts.tsx
@@ -1,142 +1,15 @@
+import { CartsItem } from "@components/carts-item";
 import styles from "./Carts.module.scss";
-import boots_white from "@icons/boots_white.png";
-import { CountBtn } from "@ui-kit/count-btn";
-import { Link } from "react-router-dom";
-import { RedButton } from "@ui-kit/red-button";
-import basket from "@icons/basket.svg";
-import { useToggleState } from "@hooks/useToggleState";
 
 export const Carts = () => {
-  const {
-    state: isDelete,
-    setTrue: handleDelete,
-    setFalse: handleAddToCart,
-  } = useToggleState();
-
   return (
     <div className={styles.cart}>
-      <ul className={styles.cart__form}>
-        <li className={styles.cart__form_item}>
-          <Link to="/product/1" className={styles.cart__form_left}>
-            <img
-              src={boots_white}
-              alt="boots"
-              className={styles.cart__form_img}
-            />
-            <div className={styles.cart__form_about}>
-              <div className={styles.cart__form_about_title}>
-                Essence Mascara Lash Princess
-              </div>
-              <div className={styles.cart__form_about_price}>$110</div>
-            </div>
-          </Link>
-          {!isDelete ? (
-            <>
-              <CountBtn onResetToCart={handleDelete} />
-              <button
-                className={styles.cart__form_delete}
-                onClick={handleDelete}
-              >
-                Delete
-              </button>
-            </>
-          ) : (
-            <button onClick={handleAddToCart}>
-              <RedButton imageSrc={basket} padding="16px" />
-            </button>
-          )}
-        </li>
-        <li className={styles.cart__form_item}>
-          <Link to="/product/1" className={styles.cart__form_left}>
-            <img
-              src={boots_white}
-              alt="boots"
-              className={styles.cart__form_img}
-            />
-            <div className={styles.cart__form_about}>
-              <div className={styles.cart__form_about_title}>
-                Essence Mascara Lash Princess
-              </div>
-              <div className={styles.cart__form_about_price}>$110</div>
-            </div>
-          </Link>
-          {!isDelete ? (
-            <>
-              <CountBtn onResetToCart={handleDelete} />
-              <button
-                className={styles.cart__form_delete}
-                onClick={handleDelete}
-              >
-                Delete
-              </button>
-            </>
-          ) : (
-            <button onClick={handleAddToCart}>
-              <RedButton imageSrc={basket} padding="16px" />
-            </button>
-          )}
-        </li>
-        <li className={styles.cart__form_item}>
-          <Link to="/product/1" className={styles.cart__form_left}>
-            <img
-              src={boots_white}
-              alt="boots"
-              className={styles.cart__form_img}
-            />
-            <div className={styles.cart__form_about}>
-              <div className={styles.cart__form_about_title}>
-                Essence Mascara Lash Princess
-              </div>
-              <div className={styles.cart__form_about_price}>$110</div>
-            </div>
-          </Link>
-          {!isDelete ? (
-            <>
-              <CountBtn onResetToCart={handleDelete} />
-              <button
-                className={styles.cart__form_delete}
-                onClick={handleDelete}
-              >
-                Delete
-              </button>
-            </>
-          ) : (
-            <button onClick={handleAddToCart}>
-              <RedButton imageSrc={basket} padding="16px" />
-            </button>
-          )}
-        </li>
-        <li className={styles.cart__form_item}>
-          <Link to="/product/1" className={styles.cart__form_left}>
-            <img
-              src={boots_white}
-              alt="boots"
-              className={styles.cart__form_img}
-            />
-            <div className={styles.cart__form_about}>
-              <div className={styles.cart__form_about_title}>
-                Essence Mascara Lash Princess
-              </div>
-              <div className={styles.cart__form_about_price}>$110</div>
-            </div>
-          </Link>
-          {!isDelete ? (
-            <>
-              <CountBtn onResetToCart={handleDelete} />
-              <button
-                className={styles.cart__form_delete}
-                onClick={handleDelete}
-              >
-                Delete
-              </button>
-            </>
-          ) : (
-            <button onClick={handleAddToCart}>
-              <RedButton imageSrc={basket} padding="16px" />
-            </button>
-          )}
-        </li>
-      </ul>
+      <div className={styles.cart__form}>
+        <CartsItem />
+        <CartsItem />
+        <CartsItem />
+        <CartsItem />
+      </div>
     </div>
   );
 };
